e99bf7afedec82561386d6b9f78a2363c79bbb1d
Adrian Letchford <me@dradrian.com>
2023-03-29 08:50:51 +0100

    Remove build envs

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ echo "Executing build.sh"
 
 if [[ ${VERCEL_ENV} == "production" ]]; then
     echo "Production deployment with VERCEL_URL: '$VERCEL_URL'";
-    echo "Production deployment with BASE_URL: '$BASE_URL'";
     hugo --gc
 else
     echo "Not production deployment with VERCEL_URL: '$VERCEL_URL'";
