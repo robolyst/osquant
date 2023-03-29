020b70ba8eef06a6effead5e728a5825ac3230fb
Adrian Letchford <me@dradrian.com>
2023-03-29 08:48:35 +0100

    Extra logging

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Executing build.sh"
+
 if [[ ${VERCEL_ENV} == "production" ]]; then
     echo "Production deployment with VERCEL_URL: '$VERCEL_URL'";
+    echo "Production deployment with BASE_URL: '$BASE_URL'";
     hugo --gc
 else
     echo "Not production deployment with VERCEL_URL: '$VERCEL_URL'";
